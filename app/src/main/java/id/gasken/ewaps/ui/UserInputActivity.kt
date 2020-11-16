56b72dcd8f2a33a9f916090fc7791468c121ca67
AriefRahmanW <rivurjr@gmail.com>
2020-11-16 15:45:49 +0700

    add google maps to user input

--- app/src/main/java/id/gasken/ewaps/ui/UserInputActivity.kt
+++ app/src/main/java/id/gasken/ewaps/ui/UserInputActivity.kt
@@ -2,16 +2,38 @@ package id.gasken.ewaps.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.CameraUpdateFactory
+import id.gasken.ewaps.R
 import id.gasken.ewaps.databinding.ActivityUserInputBinding
 
-class UserInputActivity : AppCompatActivity() {
+
+class UserInputActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var binding: ActivityUserInputBinding
+
+    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityUserInputBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
